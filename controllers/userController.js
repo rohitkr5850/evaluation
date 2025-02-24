aa84ec1c63e6a64dc5ba720c7ffade23d4b058ce
rohitkr5850 <rroy854100@gmail.com>
2025-02-24 13:55:14 +0530

    U

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,9 +15,21 @@ exports.getUserViews = async(req ,res) =>
     {
         try {
             const recipes = await
-           Recipe.find
+           Recipe.find({author:req.params.userId});
+           const totalViews = recipes.reduce((sum , recipe) => sum + recipe.views, 0);
+           res.json({totalViews});
         }catch(err){
             res.status(500).json({error:err.message});
         }
     };
-    
+    exports.getUserTopRecipe = async(req ,res) => 
+        {
+            try {
+                const recipes = await
+               Recipe.findOne({author:req.params.userId});
+               const totalViews = recipes.sort("-views");
+               res.json({recipe});
+            }catch(err){
+                res.status(500).json({error:err.message});
+            }
+        };
